157dae8346d9437861648d8513a3df0aa23d9f93
macbook <harisgorcevic1000@gmail.com>
2023-10-22 01:29:50 +0200

    Rad

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./products.css";
+import axios from "axios";
 function ProductsPage() {
-  return <div>ProductsPage</div>;
+  const [data, setData] = useState([]);
+  function getData() {
+    axios
+      .get(`https://dummyjson.com/products`)
+      .then((data) => console.log(data.data.products));
+  }
+  console.log(data);
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className="main">
+      <div className="card"></div>
+    </div>
+  );
 }
 
 export default ProductsPage;
